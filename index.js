f0dc26e8703e6954abdab13ecee2501bf1063b62
nouamh231 <nouamh231@studentmail.oru.se>
2026-01-21 07:49:33 +0100

    index.js cors change

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [
+    "http://localhost:5173",
+    "https://portfolio-frontendnew.vercel.app"
+  ],
   methods: ['GET', 'POST'],
   credentials: true
 }));
